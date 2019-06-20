ecb4d2f22393cfe687191ec9f239ca82d0a969d7
Vitalii Kalaida <nevadskiy@gmail.com>
2019-06-20 19:52:03 +0300

    Fix aliases

--- aliases.sh
+++ aliases.sh
@@ -1,8 +1,13 @@
-alias own='sudo chown -R $(id -u):$(id -g)'
-
-alias artisan='docker-compose exec --user "$(id -u):$(id -g)" php-cli php artisan'
+# Execute commands from any container
 alias from='docker-compose exec'
 
+# Change owner for current user
+alias own='sudo chown -R $(id -u):$(id -g)'
+
+# Run artisan commands
+alias artisan='docker-compose exec --user "$(id -u):$(id -g)" php-cli php artisan'
+
+# Simple testing aliases
 alias test='docker-compose exec php-cli vendor/bin/phpunit'
 alias tf='docker-compose exec php-cli vendor/bin/phpunit --filter'
 alias tfc='docker-compose exec php-cli vendor/bin/phpunit  --coverage-html tests/report --filter'
