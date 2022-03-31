62bcd0f888d89ce59dbac2802fad27f587173c16
Nevadskiy <nevadskiy@gmail.com>
2022-03-31 22:29:58 +0300

    Add base swoole script

---
+++ scripts/swoole.sh
@@ -0,0 +1,9 @@
+# Install swoole as pecl extension.
+
+# Run swoole installation commands.
+
+# Update api-gateway configuration.
+
+# Update api-app configuration to run octane command.
+
+# Remove index.php from public (check if it is required).
