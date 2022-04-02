1fb2043e7f1b11bf26424cd5ad18f29796e2bb79
Nevadskiy <nevadskiy@gmail.com>
2022-04-02 18:48:59 +0300

    Update clear.sh script

--- client/scripts/clear.sh
+++ client/scripts/clear.sh
@@ -1,3 +1,25 @@
-echo "$(pwd)"
+#!/bin/bash
 
-rm -v !("docker-compose.yml|.env|stubs|scripts")
+# Stop all containers
+docker-compose down
+
+# Remove folders
+sudo rm -rf ./.nuxt
+sudo rm -rf ./composables
+sudo rm -rf ./layouts
+sudo rm -rf ./node_modules
+sudo rm -rf ./pages
+sudo rm -rf ./plugins
+sudo rm -rf ./public
+
+# Remove files
+sudo rm -f .gitignore
+sudo rm -f app.vue
+sudo rm -f nuxt.config.ts
+sudo rm -f package.json
+sudo rm -f README.md
+sudo rm -f tsconfig.json
+sudo rm -f yarn.lock
+
+# Print final message
+echo "Done"
