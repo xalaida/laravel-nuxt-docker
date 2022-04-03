2288de8e3059cc6e42ac4ea205ccf0b9fe04d35a
Nevadskiy <nevadskiy@gmail.com>
2022-04-03 19:48:19 +0300

    Update installation script

--- api/install.sh
+++ api/install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# Build base container
+docker build ./.docker/dev/base -t api-base
+
 # Build the API containers
 docker-compose build
 
 # Init a new Laravel app
-docker-compose run app composer create-project --prefer-dist laravel/laravel src
+docker-compose run --rm app composer create-project --prefer-dist laravel/laravel src
 
 # Set ownership of the app to the current user
 sudo chown -R "$(id -u)":"$(id -g)" ./src
-# Move all files and directories up one level
+
+# Move app from the src directory to the current directory
 # TODO: rewrite without terminal errors
 mv src/* src/.* .
 
@@ -21,23 +25,20 @@ if [ ! -f ./.env ]; then
 fi
 
 # Generate the app key
-docker-compose run app php artisan key:generate --ansi
+docker-compose run --rm app php artisan key:generate --ansi
 
 # Install Swoole
-docker-compose run app composer require laravel/octane
-docker-compose run --user "$(id -u)":"$(id -g)" app php artisan octane:install --server=swoole
+docker-compose run --rm app composer require laravel/octane
+docker-compose run --rm --user "$(id -u)":"$(id -g)" app php artisan octane:install --server=swoole
 
 # Install breeze
 INSTALL_BREEZE=true
 
-# TODO: install it by condition
+# TODO: install it conditionally
 if [ ${INSTALL_BREEZE} ]; then
-    docker-compose run app composer require laravel/breeze --dev
-    docker-compose run --user "$(id -u)":"$(id -g)" app php artisan breeze:install api
+    docker-compose run --rm app composer require laravel/breeze --dev
+    docker-compose run --rm --user "$(id -u)":"$(id -g)" app php artisan breeze:install api
 fi
-
-# Stop containers
-docker-compose down
 
 # Print the final message
 echo "Laravel has been installed"
