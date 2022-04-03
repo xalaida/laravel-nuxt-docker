53f6f6ba9e98fa865793e2be0b0b8870a032931f
Nevadskiy <nevadskiy@gmail.com>
2022-04-03 19:19:20 +0300

    Remove old files

--- scripts/swoole.sh
+++
@@ -1,9 +0,0 @@
-# Install swoole as pecl extension.
-
-# Run swoole installation commands.
-
-# Update api-gateway configuration.
-
-# Update api-app configuration to run octane command.
-
-# Remove index.php from public (check if it is required).
