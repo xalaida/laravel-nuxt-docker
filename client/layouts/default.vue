f67878d86c46ea71574ed977ceb1d55d909cd017
nevadskiy <nevadskiy@gmail.com>
2022-04-09 20:11:36 +0300

    Update layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,20 +1,23 @@
 <template>
-	<header>
-		<nav>
-			<ul>
-				<li>
-					<NuxtLink to="/">Home</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="/about">About</NuxtLink>
-				</li>
-			</ul>
-		</nav>
-	</header>
-	<main>
-		<slot />
-	</main>
-	<footer>
-		nevadskiy &copy; 2022
-	</footer>
+	<div>
+		<header>
+			<h2>Fixed layout</h2>
+			<nav>
+				<ul>
+					<li>
+						<NuxtLink to="/">Home</NuxtLink>
+					</li>
+					<li>
+						<NuxtLink to="/about">About</NuxtLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<slot />
+		</main>
+		<footer>
+			nevadskiy &copy; 2022
+		</footer>
+	</div>
 </template>
