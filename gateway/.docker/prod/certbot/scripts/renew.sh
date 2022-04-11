d7eb2d57d73db4d2b223ed6cfbca4a37992f2f48
nevadskiy <nevadskiy@gmail.com>
2022-04-11 21:21:50 +0300

    Update gateway config

--- gateway/.docker/prod/certbot/scripts/renew.sh
+++ gateway/.docker/prod/certbot/scripts/renew.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# Renew containers (or maybe use --post-hook)
-docker-compose run --entrypoint certbot certbot renew
+echo 'here'
 
-# Reload nginx (do it using docker_api)
-docker-compose exec -T nginx nginx -s reload
+#certbot renew
+
+#certbot renew --pre-hook "service nginx stop" --post-hook "service nginx start"
+
+## Renew containers (or maybe use --post-hook)
+#docker-compose run --entrypoint certbot certbot renew
+#
+## Reload nginx (do it using docker_api)
+#docker-compose exec -T nginx nginx -s reload
---
+++ gateway/README.md
@@ -0,0 +1,9 @@
+# TODO
+
+- [ ] add hook to automatically reload nginx proxy container 
+- [ ] add .env variable to different hosts, staging server and others
+- [ ] set up crontab
+- [ ] add possibility to generate 4096-bit key
+- [ ] add install.sh script that copies .env file which is not ignored by git
+- [ ] make nginx work even when some hosts are not available
+- [ ] add dev env
