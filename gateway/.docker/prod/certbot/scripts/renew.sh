4dea749b2d6ad2fbc4e955d8f5ccaaeddf8bd52c
nevadskiy <nevadskiy@gmail.com>
2022-04-12 23:57:36 +0300

    Update script

--- gateway/.docker/prod/certbot/scripts/renew.sh
+++ gateway/.docker/prod/certbot/scripts/renew.sh
@@ -3,11 +3,11 @@
 echo 'Start renewing certificates'
 certbot renew --webroot-path /var/www/acme --staging
 
-echo 'Restarting gateway service'
+#echo 'Restarting gateway service'
 
 # Extract into certbot hook (certbot renew --pre-hook "service nginx stop" --post-hook "service nginx start")
 # TODO: get container name from ENV variable
-curl --unix-socket /var/run/docker.sock -X POST http://v1.41/containers/reverse-proxy/restart
+#curl --unix-socket /var/run/docker.sock -X POST http://v1.41/containers/reverse-proxy/restart
 
 # TODO: replace restart command above with 'nginx -s reload' command using 'exec' API (commands below does not work)
 # curl --unix-socket /var/run/docker.sock -X POST http://v1.41/containers/reverse-proxy/exec -d '{"Cmd": ["nginx", "-s", "reload"]}' -H "Content-Type: application/json"
