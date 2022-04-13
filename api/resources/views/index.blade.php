14fb4db4fd1cbf47b1aff2fb9219a6ef502ab7cd
nevadskiy <nevadskiy@gmail.com>
2022-04-13 23:44:17 +0300

    Add index page

---
+++ api/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello from API</title>
+</head>
+<body>
+    <h1>Hello from API</h1>
+    <img src="{{ asset('img/car.jpg') }}" alt="Car">
+</body>
+</html>
