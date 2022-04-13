b8d90416de00faf26468dddca7c68f8a08a4bf4a
nevadskiy <nevadskiy@gmail.com>
2022-04-13 14:47:33 +0300

    Update gateway

--- gateway/.docker/prod/certbot/scripts/renew.sh
+++ gateway/.docker/prod/certbot/scripts/renew.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-echo 'Start renewing certificates'
-# certbot renew --non-interactive --no-random-sleep-on-renew --webroot --webroot-path /var/www/acme --force-renewal --staging --max-log-backups 20
-
-#echo 'Restarting gateway service'
-
-# Extract into certbot hook (certbot renew --pre-hook "service nginx stop" --post-hook "service nginx start")
-# TODO: get container name from ENV variable
-#curl --unix-socket /var/run/docker.sock -X POST http://v1.41/containers/reverse-proxy/restart
-
-# TODO: replace restart command above with 'nginx -s reload' command using 'exec' API (commands below does not work)
-# TRY to add this: {"Detach": false, "Tty": false}
-# curl --unix-socket /var/run/docker.sock -X POST http://v1.41/containers/reverse-proxy/exec -d '{"Cmd": ["nginx", "-s", "reload"]}' -H "Content-Type: application/json"
-# curl --unix-socket /var/run/docker.sock -X POST http://v1.41/containers/reverse-proxy/exec -d '{"Cmd": ["/bin/sh", "-c", "nginx -s reload"]}' -H "Content-Type: application/json"
-
-echo 'Certificates have been renewed'
+certbot renew \
+    --non-interactive \
+    --no-random-sleep-on-renew \
+    --webroot \
+    --webroot-path /var/www/acme \
+    --force-renewal \
+    --staging \
