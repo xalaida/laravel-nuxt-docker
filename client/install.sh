1ba7cef760a7a878f9a512c90d72f47c4fadd685
Nevadskiy <nevadskiy@gmail.com>
2023-01-29 17:05:48 +0200

    Update installation script

--- client/install.sh
+++ client/install.sh
@@ -1,44 +1,47 @@
 #!/bin/bash
 
-install_nuxt() {
-  local INSTALL_DIRECTORY=src
+# Specify variables
+ENV_DIST_FILE=.env.dev
+ENV_FILE=.env
+DOCKER_NETWORK=gateway
+COMPOSE_FILE=docker-compose.dev.yml
+CONTAINER=app
+TEMP_INSTALL_DIRECTORY=src
 
-  # Init a new Nuxt app into a temporary directory
-  docker-compose -f docker-compose.dev.yml run --rm --no-deps \
-    --user "$(id -u)":"$(id -g)" app \
-    npx nuxi init ${INSTALL_DIRECTORY}
-
-  # Set ownership of the temporary directory to the current user
-  sudo chown -R "$(id -u)":"$(id -g)" ./${INSTALL_DIRECTORY}
-
-  # Move everything from the temporary directory to the current directory
-  mv ${INSTALL_DIRECTORY}/* ${INSTALL_DIRECTORY}/.* .
-
-  # Remove the temporary directory
-  rm -r ${INSTALL_DIRECTORY}
-}
-
-# Copy .env file
-if [ ! -f ./.env ]; then
-    cp ./.env.dev ./.env
+# Copy .env file from the dist
+if [ ! -f $ENV_FILE ]; then
+    cp $ENV_DIST_FILE $ENV_FILE
 fi
 
+#todo provide --nuxt2 option that automatically copies stubs
+
 # Create shared gateway network
-docker network create gateway
+docker network create $DOCKER_NETWORK
 
 # Build containers
-make build
+docker compose -f $COMPOSE_FILE build
 
-# Install Nuxt framework
-install_nuxt
+# Init a new Nuxt app into a temporary directory
+docker-compose -f $COMPOSE_FILE run --rm --no-deps \
+  --user "$(id -u)":"$(id -g)" $CONTAINER \
+  npx nuxi init $TEMP_INSTALL_DIRECTORY
+
+# Set ownership of the temporary directory to the current user
+sudo chown -R "$(id -u)":"$(id -g)" $TEMP_INSTALL_DIRECTORY
+
+# Move everything from the temporary directory to the current directory
+mv $TEMP_INSTALL_DIRECTORY/* $TEMP_INSTALL_DIRECTORY/.* .
+
+# Remove the temporary directory
+rm -r $TEMP_INSTALL_DIRECTORY
 
 # Install packages
-docker-compose -f docker-compose.dev.yml run --rm --no-deps \
-  --user "$(id -u)":"$(id -g)" app \
+docker-compose -f $COMPOSE_FILE run --rm --no-deps \
+  --user "$(id -u)":"$(id -g)" $CONTAINER \
   yarn install
 
 # Start containers
-make up
+docker-compose -f $COMPOSE_FILE up -d
 
 # Print the final message
 echo "The client app has been installed and run on http://localhost:3000."
