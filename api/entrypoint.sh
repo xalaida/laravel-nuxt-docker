6e0b75f7c92ab5d9ec9ed94346c443c1d725ac99
nevadskiy <nevadskiy@gmail.com>
2024-01-02 23:48:10 +0200

    Move commands to build stage

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,20 +1,5 @@
-# Exit immediately on any non-zero exit status
+# Exit immediately on any following non-zero exit status
 set -e
-
-# Optimize config loading
-php artisan config:cache
-
-# Optimizing route loading
-php artisan route:cache
-
-# Optimizing view loading
-php artisan view:cache
-
-# Optimizing event loading
-php artisan event:cache
-
-# Optimize framework loading
-php artisan optimize
 
 # Run database migrations
 php artisan migrate --force
